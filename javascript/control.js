52124124e2cf0feb8316a05929932ba78eb56a2a
vhzb <vhugoz1@hotmail.com>
2021-09-17 13:19:12 -0300

    Concluído o nav e a navegação vertical. Falta adicionar a seção de contatos na página.

---
+++ javascript/control.js
@@ -0,0 +1,72 @@
+
+$().ready(function(){
+    // animação menu descendo
+    // utilizando slide up e down do Jquery para fazer o efeito de transição
+    $('.has-intern').each(function(index, obj){
+        $(obj).on({
+            mouseover : function(){
+                $(obj).find('.inner-list').slideDown();
+            },
+            mouseleave : function(){
+                $(obj).find('.inner-list').slideUp();
+            }
+        })
+    })
+
+    // navegação vertical
+    // Fazendo slideup do nav antes da navegação vertical para não causar conflito
+    var ids = ['about-page', 'page', 'about-me', 'me', 'objectives', 'languages', 'certificates', 'contact'];
+    $('.v-nav').each(function(index, obj){
+        $(obj).click(function(){
+            $(obj).find('.inner-list').slideUp(200, function(){
+                $('html,body').animate({
+                    scrollTop: $('#'+ids[index]).offset().top
+                }, 1500);
+            })
+        })
+    })
+
+    // script para mudança de certificado pelos botões
+    var previousObj;
+    $('#certificate').hide();
+    $('#c-div div').each(function(i, obj){
+        $(obj).find('.btn').each(function(i2, obj2){
+            $(obj2).on({
+                mouseover : function(){
+                    this.style.cursor = 'pointer';
+                }, click : function(){
+                    var fileimg = 'imagens/certificate/';
+                    var filepdf = 'docs/'
+                    switch($(obj).attr('id')){
+                        case 'c-udemy':
+                            fileimg = fileimg + 'udemyc';
+                            filepdf = filepdf + 'udemyc';
+                            break;
+                        case 'c-impacta':
+                            fileimg = fileimg + 'impactac';
+                            filepdf = filepdf + 'impactac';
+                            break;
+                        case 'c-coursera':
+                            fileimg = fileimg + 'courserac';
+                            filepdf = filepdf + 'courserac';
+                            break;
+                    }
+                    fileimg = fileimg + (i2 + 1) + '.jpg';
+                    filepdf = filepdf + (i2 + 1) + '.pdf';
+                    $('#certificate img').attr('src', fileimg);
+                    $('#certificate p').html('PDF File: <a href="' + filepdf + '" target="_blank">Click Here!</a>');
+
+                    if (obj2 == previousObj){
+                        $('#certificate').slideToggle();
+                        $(obj2).toggleClass('pressed');                
+                    } else {
+                        $('#certificate').slideDown();
+                        $(obj2).addClass('pressed');
+                        $(previousObj).removeClass('pressed');
+                    }
+                    previousObj = obj2;
+                }
+            })
+        })
+    })
+})
